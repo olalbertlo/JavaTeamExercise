d30ed9b2b264f42679f4155b51924d9accc1c0f2
chenliangyeam <cliangyen@gmail.com>
2025-04-25 23:49:40 +0800

    feat: third version-print the third member name

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,6 +3,7 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257060\"\n姓名：\"謝誌評\"");
         System.out.println("\"學號：\"01257107\"\n姓名：\"李知為\"") ;
+		System.out.println("\"學號：\"01257031\"\n姓名：\"陳亮諺\"");
     }
 }
 
