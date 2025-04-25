48af7192f39420b1bf74dddc170b4cc04b869456
WesleyLee093 <wesley931205isgood@gmail.com>
2025-04-25 23:03:04 +0800

    feat: second version-print the second member name

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,7 +2,7 @@ public class FirstClass4Team {
     public static void main(String[] args){
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257060\"\n姓名：\"謝誌評\"");
-        System.out.println("\"學號：\"01257107\"\n姓名：\"李知為\"");
+        System.out.println("\"學號：\"01257107\"\n姓名：\"李知為\"") ;
     }
 }
 
