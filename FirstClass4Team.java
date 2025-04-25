a90e507d8577a71f9920bc3b558081464ca31ae9
olalbertlo <albert0915a@gmail.com>
2025-04-25 21:51:45 +0800

    feat: first version-print the first member name

---
+++ FirstClass4Team.java
@@ -0,0 +1,7 @@
+public class FirstClass4Team {
+    public static void main(String[] args){
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01257060\"\n姓名：\"謝誌評\"");
+    }
+}
+
